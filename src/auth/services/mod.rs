b1b897e280c0b551bcb881d6639430cbf06f05ef
Aberkingaliev <erkingaliev21@gmail.com>
2023-05-01 14:55:42 +0600

    Add validation rule module, update error handler

--- src/auth/services/mod.rs
+++ src/auth/services/mod.rs
@@ -1,5 +1,10 @@
 use crate::{
-    error_responder::ApiErrorResponse,
+    auth::api_errors::{
+        INVALID_PASSWORD, PASSWORD_HASHING_ERROR, PASSWORD_VERIFICATION_ERROR,
+        TOKEN_GENERATION_ERROR, TOKEN_VALIDATION_ERROR, UNEXPECT_DB_ERROR, USER_ALREADT_EXIST,
+        USER_NOT_FOUND,
+    },
+    mail::message_layouts::activation_message,
     mail::services::MailService,
     token::models::TokenDto,
     token::services::{JwtService, TokenBundle, TokenService},
@@ -10,10 +15,6 @@ use bcrypt::{hash, verify};
 use diesel::PgConnection;
 use serde::{Deserialize, Serialize};
 
-use super::api_errors::{
-    failed_create_user, invalid_password, user_already_registred, user_not_found,
-};
-
 #[derive(Serialize, Deserialize)]
 pub struct AuthResponse {
     pub tokens: TokenBundle,
@@ -22,6 +23,24 @@ pub struct AuthResponse {
 
 pub struct AuthService<'a> {
     connection: &'a mut PgConnection,
+}
+
+pub enum AuthLoginResult {
+    Ok(AuthResponse),
+    NotFound(&'static str),
+    InvalidPassword(&'static str),
+    UnexpectedError(&'static str),
+}
+
+pub enum AuthRegistrationResult {
+    Ok(AuthResponse),
+    AlreadyRegistred(&'static str),
+    UnexpectedError(&'static str),
+}
+
+pub enum AuthRefreshResult {
+    Ok(AuthResponse),
+    UnexpectedError(&'static str),
 }
 
 impl<'a> From<&'a mut PgConnection> for AuthService<'a> {
@@ -34,16 +53,19 @@ impl<'a> AuthService<'a> {
     pub async fn user_registration(
         &'a mut self,
         user: UserRegistrationDto,
-    ) -> Result<AuthResponse, ApiErrorResponse> {
+    ) -> AuthRegistrationResult {
         let current_user = UserService::new(self.connection)
             .check_user_by_email(&user.email)
             .await;
 
         if current_user {
-            return Err(user_already_registred());
+            return AuthRegistrationResult::AlreadyRegistred(USER_ALREADT_EXIST);
         }
 
-        let hashed_password = hash(&user.password, 10).expect("Error: Hashing Password");
+        let hashed_password = match hash(&user.password, 10) {
+            Ok(password) => password,
+            Err(_) => return AuthRegistrationResult::UnexpectedError(PASSWORD_HASHING_ERROR),
+        };
         let activation_link = uuid::Uuid::new_v4().to_string();
         let registration_dto = UserRegistrationDto {
             password: hashed_password,
@@ -55,16 +77,8 @@ impl<'a> AuthService<'a> {
             .await
         {
             Ok(created_user) => {
-                let activation_link = &created_user.activation_link;
-                let username = &created_user.username;
-                let html_body = format!(
-                "<html><body><p>Dear {},</p>\
-                <p>Thank you for creating an account. Please click on the following link to activate your account:</p>\
-                <p><a href=\"http://localhost:8000/api-v1/activate/{}\">http://localhost:8000/api-v1/activate/{}</a></p>\
-                <p>Best regards,</p>\
-                <p>The FileVerify Team</p></body></html>",
-                username, activation_link, activation_link
-                );
+                let html_body =
+                    activation_message(&created_user.name, &created_user.activation_link);
                 let mut message = MailService {
                     receiver: &created_user.email.as_str(),
                     subject: "Account activation",
@@ -73,14 +87,15 @@ impl<'a> AuthService<'a> {
                 message.send().await;
                 created_user
             }
-            Err(_) => return Err(failed_create_user()),
+            Err(_) => return AuthRegistrationResult::UnexpectedError(UNEXPECT_DB_ERROR),
         };
         let user_payload = UserPayloadDto {
             ..registration_dto.into()
         };
-        let generated_tokens = JwtService::generate_tokens(&user_payload)
-            .await
-            .expect("Error during token generation");
+        let generated_tokens = match JwtService::generate_tokens(&user_payload).await {
+            Ok(tokens) => tokens,
+            Err(_) => return AuthRegistrationResult::UnexpectedError(TOKEN_GENERATION_ERROR),
+        };
         let new_token = TokenDto {
             user_id: &created_user.id,
             refresh_token: &generated_tokens.refresh_token,
@@ -90,41 +105,40 @@ impl<'a> AuthService<'a> {
             .create_token(&new_token)
             .await;
 
-        Ok(AuthResponse {
+        AuthRegistrationResult::Ok(AuthResponse {
             tokens: generated_tokens,
             user: user_payload,
         })
     }
 
-    pub async fn user_login(
-        &'a mut self,
-        user_credintals: UserLoginDto,
-    ) -> Result<AuthResponse, ApiErrorResponse> {
+    pub async fn user_login(&'a mut self, user_credintals: UserLoginDto) -> AuthLoginResult {
         let current_user = match UserService::new(self.connection)
             .get_user_by_email(&user_credintals.email)
             .await
         {
             Ok(user) => user,
-            Err(_) => return Err(user_not_found()),
+            Err(_) => return AuthLoginResult::NotFound(USER_NOT_FOUND),
         };
 
-        let is_password_valid = verify(user_credintals.password, &current_user.password)
-            .expect("Error verifying password");
-
-        if !is_password_valid {
-            return Err(invalid_password());
-        }
+        match verify(user_credintals.password, &current_user.password) {
+            Ok(bool) => {
+                if !bool {
+                    return AuthLoginResult::InvalidPassword(INVALID_PASSWORD);
+                }
+            }
+            Err(_) => return AuthLoginResult::UnexpectedError(PASSWORD_VERIFICATION_ERROR),
+        };
 
         let payload = UserPayloadDto {
             name: current_user.name,
-            username: current_user.username,
             email: current_user.email,
             is_activated: current_user.is_activated,
         };
 
-        let generated_tokens = JwtService::generate_tokens(&payload)
-            .await
-            .expect("Token generation error");
+        let generated_tokens = match JwtService::generate_tokens(&payload).await {
+            Ok(tokens) => tokens,
+            Err(_) => return AuthLoginResult::UnexpectedError(TOKEN_GENERATION_ERROR),
+        };
 
         if let Ok(token) = TokenService::new(self.connection)
             .get_token_by_user_id(&current_user.id)
@@ -144,26 +158,29 @@ impl<'a> AuthService<'a> {
                 .await;
         }
 
-        Ok(AuthResponse {
+        AuthLoginResult::Ok(AuthResponse {
             tokens: generated_tokens,
             user: payload,
         })
     }
 
-    pub async fn refresh_user(
-        &'a mut self,
-        refresh_token: String,
-    ) -> Result<AuthResponse, ApiErrorResponse> {
-        let current_token = TokenService::new(self.connection)
+    pub async fn refresh_user(&'a mut self, refresh_token: String) -> AuthRefreshResult {
+        let current_token = match TokenService::new(self.connection)
             .get_token_by_refresh(refresh_token)
             .await
-            .expect("Error during find token");
+        {
+            Ok(token) => token,
+            Err(_) => return AuthRefreshResult::UnexpectedError(UNEXPECT_DB_ERROR),
+        };
 
-        let validation_token = JwtService::validate_token(&current_token.refresh_token)
-            .expect("Error validating token");
-        let generated_tokens = JwtService::generate_tokens(&validation_token)
-            .await
-            .expect("Error generating token");
+        let validation_token = match JwtService::validate_token(&current_token.refresh_token) {
+            Ok(payload) => payload,
+            Err(_) => return AuthRefreshResult::UnexpectedError(TOKEN_VALIDATION_ERROR),
+        };
+        let generated_tokens = match JwtService::generate_tokens(&validation_token).await {
+            Ok(tokens) => tokens,
+            Err(_) => return AuthRefreshResult::UnexpectedError(TOKEN_GENERATION_ERROR),
+        };
 
         if let Ok(user) = UserService::new(self.connection)
             .get_user_by_id(&current_token.user_id)
@@ -182,7 +199,7 @@ impl<'a> AuthService<'a> {
                 .await
         };
 
-        Ok(AuthResponse {
+        AuthRefreshResult::Ok(AuthResponse {
             tokens: generated_tokens,
             user: validation_token,
         })
